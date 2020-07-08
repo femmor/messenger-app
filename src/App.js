0381326ad40422a729c48afdfd6fbde16bd209ef
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-08 16:24:36 -0500

    create the Message component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css'
 
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 
+// Components
+import Message from "./components/Message"
+
 function App() {
 
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("")
 
   const sendMessage = (e) => {
     e.preventDefault()
@@ -24,14 +28,14 @@ function App() {
           <InputLabel>Enter a message</InputLabel>
           <Input value={input} onChange={(e) => setInput(e.target.value)} />
           {/* Button */}
-      <Button type="submit" variant="contained" color="primary" disabled={!input} onClick={sendMessage}>Send Message</Button>
+          <Button type="submit" variant="contained" color="primary" disabled={!input} onClick={sendMessage}>Send Message</Button>
         </FormControl>
       </form>
 
       {/* Messages */}
       <div>
         {messages.map(message => (
-          <p key={message}>{message}</p>
+          <Message text={message} key={message}/>
         ))}
       </div>
      </div>
---
+++ src/components/Message.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Message = ({ text }) => {
+    return (
+        <>
+            <h3>
+                {text}
+            </h3>
+        </>
+    );
+}
+
+export default Message;
