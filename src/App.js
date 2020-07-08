b066de5a95e0522f885ef29f9e998824499418d1
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-08 15:36:10 -0500

    setup the input and message state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["Hi!", "What's up?"])
+
+  const sendMessage = (e) => {
+    // Store messages
+    setMessages([...messages, input])
+    setInput("")
+  }
+
   return (
     <div className="App">
-      Working!
+      <h1>Messenger App</h1>
+      {/* Input Field */}
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+
+      {/* Button */}
+      <button onClick={sendMessage}>Send message</button>
+
+      {/* Messages */}
+      <div>
+        
+      </div>
      </div>
   );
 }
