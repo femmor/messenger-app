4f7be2e54b00099282c21b5f2422854875c0bfd3
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-08 16:11:28 -0500

    implement materialize ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.css'
+
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 
 function App() {
 
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState(["Hi!", "What's up?"])
+  const [messages, setMessages] = useState([])
 
   const sendMessage = (e) => {
+    e.preventDefault()
     // Store messages
     setMessages([...messages, input])
     setInput("")
@@ -15,15 +18,21 @@ function App() {
   return (
     <div className="App">
       <h1>Messenger App</h1>
-      {/* Input Field */}
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-
-      {/* Button */}
-      <button onClick={sendMessage}>Send message</button>
+      <form>
+        {/* Input Field */}
+        <FormControl>
+          <InputLabel>Enter a message</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+          {/* Button */}
+      <Button type="submit" variant="contained" color="primary" disabled={!input} onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+      </form>
 
       {/* Messages */}
       <div>
-        
+        {messages.map(message => (
+          <p key={message}>{message}</p>
+        ))}
       </div>
      </div>
   );
